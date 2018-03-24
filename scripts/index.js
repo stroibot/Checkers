84a1cf6dc3262c592eac36f0077a2793aa8293a1
Roman Romanovskiy <stroibot@gmail.com>
2018-03-24 11:14:00 +0300

    Removed an annoying blink feature of Message Box

--- scripts/index.js
+++ scripts/index.js
@@ -10,24 +10,3 @@ window.onload = () => {
     // Initialize the game
     gameManager.Initialize();
 };
-
-// This one is used to show Message Box instead of 'alert'
-window.addEventListener('click', function (e) {
-    // Get message box so we could remove it on button click
-    let messagebox = document.getElementById('outerMessageBox');
-
-    if (messagebox) {
-        if (messagebox.firstChild.classList.contains('messageBoxWithHeader')) {
-            return;
-        }
-
-        if (!messagebox.contains(e.target)) {
-            // Make it blink if we click outside the message box
-            messagebox.firstChild.classList.add('blink');
-
-            this.setTimeout(() => {
-                messagebox.firstChild.classList.remove('blink');
-            }, 500);
-        }
-    }
-});
